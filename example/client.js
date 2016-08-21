61e65667d8af8390a0b11bd3e402b6a5517d34d2
Artsy <craigspaeth@gmail.com>
2016-08-20 21:10:06 -0400

    making progress—I think there's something cool and Rails..like but hopefully without too much magic

--- example/client.js
+++ example/client.js
@@ -1,3 +1,5 @@
 import { router } from '../'
 
 router.routes()
+
+alert('hi')
--- example/server.js
+++ example/server.js
@@ -3,6 +3,5 @@ import { app, models } from '../'
 import router from './router'
 
 router.all('/api', models(m))
-const server = app(router)
 
-export default server
+export default app(router)
