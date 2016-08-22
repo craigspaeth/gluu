56b8d5960bb02783c3a9a13081831f38433b12cc
Artsy <craigspaeth@gmail.com>
2016-08-22 00:29:49 -0400

    getting closer

--- examples/crud/views/new-tweet.js
+++ examples/crud/views/new-tweet.js
@@ -4,11 +4,15 @@ import { createTweet, updateTweet } from '../controllers'
 const view = newview()
 const { div, h1, input, button, form } = view.els()
 
+view.on('componentDidMount', ({ refs }) => {
+  refs.input.focus()
+})
+
 view.render(() => {
   return div(
     h1('New Tweet'),
     form({ onSubmit: createTweet },
-      input({ onChange: updateTweet }),
+      input({ onChange: updateTweet, ref: 'input' }),
       button('Submit')))
 })
 
